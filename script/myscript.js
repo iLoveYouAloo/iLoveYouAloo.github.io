474f086e490c9cec4c6c6b7dc23a574cf11dc042
drkknght <vitobnd79@gmail.com>
2024-02-14 22:06:22 +0530

    commit

--- script/myscript.js
+++ script/myscript.js
@@ -1,4 +1,5 @@
 let isAudioPlayed = false;
+let current_slide = 0;
 
 
 
@@ -10,7 +11,7 @@ function playAudio() {
 }
 
 
-document.onclick = ()=>{
+document.getElementById('start-story').onclick = ()=>{
     if(isAudioPlayed) return ;
     playAudio();
 }
@@ -26,5 +27,45 @@ window.onload = () => {
 }
 */
 
+function make_story_array() {
+        // Using Array.from
+    const inputsArray = Array.from(document.querySelectorAll('section'));
+    return inputsArray;
+}
 
+
+document.getElementById('start-story').onclick = function() {
+    playAudio();
+    current_slide+=1;
+    document.getElementById(current_slide.toString()).style.cssText = ' position:absolute; top:0px ; transition: top 2s ease;';
+    story_slides = make_story_array();
+let elements = document.getElementsByClassName("button");
+// Loop through the collection of elements
+for (var i = 0; i < elements.length; i++) {
+    // Do something with each element, for example, change its background color
+    elements[i].style.cssText = "opacity:1; transition: opacity 2s ease";
+}
+};
+
+document.getElementById("down").onclick = function(){
+    if (current_slide === 26){
+        return;
+    }
+    current_slide+=1;
+    document.getElementById(current_slide.toString()).style.cssText = ' position:absolute; top:0px ; transition: top 2s ease;';
+    
+    console.log(typeof current_slide)
+    console.log(current_slide);
+}
+
+
+document.getElementById("up").onclick = function(){
+    if (current_slide === 0){
+        return;
+    }
+    console.log(current_slide);
+    document.getElementById(current_slide.toString()).style.cssText = ' position:absolute; top:10000px ; transition: top 4s ease;';
+    current_slide-=1;
+    
+}
 
